a4633a8e166c923ae16b874a373239e532eed6de
kunal20000 <kunalsapate2000@gmail.com>
2023-05-15 17:46:22 +0530

    Conditional Statement

---
+++ src/AssignmentPractise/ConditionalStatementEvenOrOdd.java
@@ -0,0 +1,15 @@
+package AssignmentPractise;
+
+import java.util.Scanner;
+
+public class ConditionalStatementEvenOrOdd {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        if(N%2==0){
+            System.out.println(0);
+        }else{
+            System.out.println(1);
+        }
+    }
+}
