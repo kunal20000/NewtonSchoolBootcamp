f2215ab096c267b3bfdac53ec2942ca51362b1ac
kunal20000 <kunalsapate2000@gmail.com>
2023-05-15 17:55:45 +0530

    Time Distance speed

---
+++ src/AssignmentPractise/ShinchanAndKazma.java
@@ -0,0 +1,19 @@
+package AssignmentPractise;
+
+public class ShinchanAndKazma {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int S = sc.nextInt();
+
+        int D =A-B;
+        if(D<0){
+            D=-D;
+        }
+        int time = D/S;
+        System.out.println(time);
+
+    }
+}
