4e73667b0be1f7ecb980c13df0baab92c26346f4
kunal <kunalsapate2000@gmail.com>
2024-01-02 11:20:54 +0530

    code

--- src/DSA_Sheet/binarySearchRotatedArray.java
+++ src/DSA_Sheet/binarySearchRotatedArray.java
@@ -1,8 +1,19 @@
 package DSA_Sheet;
 
 public class binarySearchRotatedArray {
+
+    public static int binarySearchRotateArray(int[] arr, int target) {
+       int low =0;
+       int high = arr.length-1;
+       
+    }
+
     public static void main(String[] args) {
-        
-        int[] arr = {2,}
+
+        int[] arr = { 4, 5, 6, 7, 0, 1, 2 };
+
+        int target = 0;
+        int result = binarySearchRotateArray(arr, target);
+        System.out.println(result);
     }
 }
